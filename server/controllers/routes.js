9cfbcf90176e9b4be72c5a99b322d1c1f737ba32
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-12-24 17:06:37 +0000

    Just making this commit to show how to wrtie a string to a csv file. The example can be found in routes.js using writefile

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { writeFile } = require('fs');
+require('dotenv').config()
+const apiKey = process.env.API_KEY;
+const axios = require('axios');
 
 router.use(cors());
 
@@ -9,6 +13,22 @@ router.use(express.json());
 
 router.get('/', (req, res) => {
     res.status(200).send(`<h1>Home page</h1>`);
+})
+
+router.get('/api/stocks', (req, res) => {
+    const getStockData = async () => {
+        const { data } = await axios.get(`https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=${apiKey}`);
+        writeFile('./data/stocks.csv', data, 'utf8', function (err) {
+            if (err) {
+              console.log('Some error occured - file either not saved or corrupted file saved.');
+            } else{
+              console.log('It saved!');
+            }
+          });
+        
+    }
+    getStockData();
+    res.send("Test");
 })
 
 
