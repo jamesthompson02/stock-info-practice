692850c5d4ae695c38d8d4788548b23e7feed3ac
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-12-26 16:02:37 +0000

    Implemented the basics of angular routing as well as an ugly navbar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { StocksComponent } from './pages/stocks/stocks.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+
+const appRoute: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'stocks', component: StocksComponent},
+  { path: 'contact', component: ContactComponent}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoute)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StocksComponent } from './pages/stocks/stocks.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    NavbarComponent,
+    StocksComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
